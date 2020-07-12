7511abdd4e6b73a2d74ddc739f232402142f6b70
Rafael Lopez <rafael.iskoat@gmail.com>
2020-07-12 14:40:10 -0500

    COnfigurando nuestro frontend con webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+  <h1>Hola que tal como les va ? </h1>,
+  document.getElementById('root'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js',
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        exclude: /(node_modules)/,
+        test: /\.js|jsx$/,
+        loader: 'babel-loader'
+      },
+
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+}

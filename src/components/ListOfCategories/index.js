addeb04ef48683ce83148d1e0be0faad8b92d677
Rafael Lopez <rafael.iskoat@gmail.com>
2020-07-17 22:16:13 -0500

    useEffect: limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,17 +4,45 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
-  useEffect(() => {
+  /**
+   * Effects
+   */
+  useEffect(() => { // efecto
     fetch('https://petgram-server-iskoat.rafael-lh.vercel.app/categories')
       .then(result => result.json())
       .then(setCategories)
-  }, [])
-  return (
-    <List>
+  }, []) // dependencia
+
+  useEffect(() => { // efecto
+    const onScroll = (e) => {
+      // aqui es donde voy a estar cambiando el estado de showFixed de true o false
+      const newShowFixed = window.scrollY > 200;
+      (showFixed !== newShowFixed) && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    // como le estamos dando un evento a todo el documento (document) si el componente se desmonta el evento seguira escuchando
+    // y esto no lo queremos asi que debemos eliminar ese evento del documento
+
+    return () => document.removeEventListener('scroll', onScroll)
+    // de esta manera estamos limpiando nuestro document de l evento scroll
+
+  }, [showFixed]) // dependencia
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(item => <Item key={item.id}><Category {...item} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }

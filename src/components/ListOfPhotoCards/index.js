417fc10690964c6e625309d942a8793531b52389
Rafael Lopez <rafael.iskoat@gmail.com>
2020-07-21 15:02:00 -0500

    Parámetros para un query con GraphQL

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
-
-const withPhotos = graphql(gql`
-  query getPhotos{
-    photos {
-      id,
-      categoryId,
-      src,
-      likes,
-      userId,
-      liked
-    }
-  }
-`)
 
 /**
  * Lo que estamos haciendo aqui es recibir por props el resultado de la query con graphql el cual es un objeto que tine la key data
  * dentro de el tiene photos que lo iniciaremos en un array vacío que por defecto todo sera un objeto vacío
  */
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   // console.log(photos)
   return (
     <ul>
@@ -31,4 +16,3 @@ const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   )
 }
 
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)

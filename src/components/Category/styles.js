618a2d36ce9ee672d96c2e1aee037ff34d024327
Rafael Lopez <rafael.iskoat@gmail.com>
2020-07-26 22:09:57 -0500

    Agregamos componente de Link de @reach/router para hacer la navegacion interna de la app ya que reach router no tiene el evento submit que tiene la etiqueta a

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
+import { Link as LinkRouter} from '@reach/router'
 
-export const Anchor = styled.a`
+// importamos el componente Link de reach router porque ese componente va haciendo push en el history
+// ese se lo pasamos a nuestro styled el cual puede recibir como parametro un componente pero no no cualquier componente si no
+// un componenete de le puedas setear la propiedad de className ya que styled componente necesita inyectarle una class
+export const Link = styled(LinkRouter)`
   display: flex;
   flex-direction: column;
   text-align: center;

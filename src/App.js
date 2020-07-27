bb180144674f8ee9d483cdb891dfd84e51fa8ca4
Rafael Lopez <rafael.iskoat@gmail.com>
2020-07-26 23:21:54 -0500

    Agregando un NavBar a nuestra app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
@@ -15,6 +16,7 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <NavBar />
     </>
   )
 }
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,24 +1,32 @@
 import styled from 'styled-components'
-import { Link as LinkRouter} from '@reach/router'
+import { Link as LinkRouter } from '@reach/router'
 
+export const Nav = styled.nav`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  z-index: 1000;
+  align-items: center;
+  background: #fcfcfc;
+  border-top: 1px solid #e0e0e0;
+  display: flex;
+  height: 50px;
+  justify-content: space-around;
+  margin: 0 auto;
+  max-width: 500px;
+`
 // importamos el componente Link de reach router porque ese componente va haciendo push en el history
 // ese se lo pasamos a nuestro styled el cual puede recibir como parametro un componente pero no cualquier componente si no
 // un componenete que le puedas setear la propiedad de className (como por ejemplo un elemento del DOM) 
 // ya que styled componente necesita inyectarle una class
 export const Link = styled(LinkRouter)`
+  align-items: center;
+  color: #888;
   display: flex;
-  flex-direction: column;
-  text-align: center;
+  height: 100%;
+  width: 100%;
+  justify-content: center;
   text-decoration: none;
-  width: 75px;
-`
-export const Image = styled.img`
-  border: 1px solid #ddd;
-  box-shadow: 0px 10px 14px rgba(0, 0, 0, .2);
-  border-radius: 100%;
-  height: auto;
-  overflow: hidden;
-  object-fit: cover;
-  height: 75px;
-  width: 75px;
 `

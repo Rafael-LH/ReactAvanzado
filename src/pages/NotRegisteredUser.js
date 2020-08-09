d0ef8012e9af7603017d9e9c2cf62586cefecfa0
Rafael Lopez <rafael.iskoat@gmail.com>
2020-08-09 16:51:10 -0500

    Agregamos Create context a nuestra app de PetGram

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import Context from '../Context'
 
 export const NotRegisteredUser = () => (
-  <h1>NotRegisteredUser</h1>
+  <Context.Consumer>
+    {
+      ({ activeAuth }) => (
+        <form onSubmit={activeAuth}>
+          <button type='submit'>Iniciar sesión</button>
+        </form>
+      )
+    }
+  </Context.Consumer>
 )

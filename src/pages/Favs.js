5378a4a0b6edd3da822df5065f873002ba070764
Rafael Lopez <rafael.iskoat@gmail.com>
2020-08-31 18:07:27 -0500

    Agregamos React Helmet

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { FavsWithQuery } from '../container/GetFavorites'
+import { Layout } from '../components/Layout'
 
 export const Favs = () => (
   <>
-    <h1>Favs</h1>
-    <FavsWithQuery />
+    <Layout title="Petgram - tus favoritos" subtitle="Aqui puedes encontrar tus favoritos">
+      <FavsWithQuery />
+    </Layout>
   </>
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
+import { Layout } from '../components/Layout'
 
-export const Home = ({ categoryId }) => {
-  return (
-    <>
-      <ListOfCategories />
-      <ListOfPhotoCards categoryId={categoryId} />
-    </>
-  )
-}
+export const Home = ({ categoryId }) => (
+  <Layout title="Tu app de fotos de mascotas" subtitle="Con petgram peudes encontrar fotos de animales domesticos muy bonitos">
+    <ListOfCategories />
+    <ListOfPhotoCards categoryId={categoryId} />
+  </Layout>
+)

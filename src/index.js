8cef61600b6c2611effa1992001d0d9933be70fb
Rafael Lopez <rafael.iskoat@gmail.com>
2020-12-07 22:00:56 -0600

    Configuramos Package.json y webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+  <h1>Hola que pasa calabaza</h1>,
+  document.getElementById('app'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (_, argv) => {
+  console.log(argv.mode);
+  return {
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+    devtool: argv.mode === 'development' ? 'source-map' : '',
+    devServer: {
+      port: 9000,
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          exclude: /(node_modules|bower_components)/,
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html',
+      })
+    ]
+  }
+}

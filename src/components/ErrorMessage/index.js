4533b7c0b02f3785dbafb329b76755919310e258
Rafael Lopez <rafael.iskoat@gmail.com>
2020-12-07 20:56:50 -0600

    Refactor

--- src/components/ErrorMessage/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ContainerError, Paragraph } from './styles'
-
-export const ErrorMessage = ({ errorMessage = 'Ha ocurrido algun error' }) => (
-  <ContainerError>
-    <Paragraph>{errorMessage}</Paragraph>
-  </ContainerError>
-)
--- src/pages/NotFound.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const NotFound = () => (
-  <h1>Esta pagína no existe! ☹️</h1>
-)
-export default NotFound

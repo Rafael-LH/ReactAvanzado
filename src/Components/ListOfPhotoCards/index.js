a55616a37cc8bcbd7f0cc1d65167736eb5918965
Rafael Lopez <rafael.iskoat@gmail.com>
2020-12-16 16:23:34 -0600

    Agregamos nuestro logo de PetGram

---
+++ src/Components/ListOfPhotoCards/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PhotoCard } from '@components/PhotoCard'
+
+export const ListOfPhotoCards = () => {
+  return (
+    <ul>
+      {
+        [1, 2, 3, 4, 5].map(item => <PhotoCard key={item} />)
+      }
+    </ul>
+  )
+}

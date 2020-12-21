f8ca2de7584041daa4808d3b18a349e2cc7634f4
Rafael Lopez <rafael.iskoat@gmail.com>
2020-12-20 22:33:22 -0600

    Agregamos PolyFill de IntersectionObserver con un dinamic import para que solo llame el PolyFill de intersection-observer y no lo tenga que llamar siempre ERPPA-473 - Crear Baja de Clave de Agentes

--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -1,22 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder } from "react-icons/md";
+import { useIntersectionObserver } from "@hooks/useIntersectionObserver";
+
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_hamsters.jpg'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const articleRef = useRef(null)
-  const [show, setShow] = useState(false)
-
-  useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0]
-      console.log(isIntersecting);
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect() // lo que hacemos es desconectar el observador para que deje de observar
-      }
-    })
-    observer.observe(articleRef.current)
-  }, [articleRef])
+  const { show } = useIntersectionObserver(articleRef)
 
   return (
     <Article ref={articleRef}>
---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+export const useIntersectionObserver = (articleRef) => {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    // Hacemos lo siguiente porque siempre queremos retornar una promesa
+    Promise.resolve( // y de esta manera siempre retornamos una promesa
+      // validamos si nuestro browser contiene por defecto IntersectionObserver
+      // o tenemos que utilizar un Polyfill que es intersection-observer
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+      // si no lo soparta con un dinamic import traemos a nuestro PolyFill de intersection-observer
+    ).then((IntersectionObserver) => {
+      const observer = new IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect() // lo que hacemos es desconectar el observador para que deje de observar
+        }
+      })
+      observer.observe(articleRef.current)
+    }, [articleRef])
+  })
+
+  return { show }
+}

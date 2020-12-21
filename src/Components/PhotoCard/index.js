2f73f189c463ad6ed8218b8291d0c902e22af053
Rafael Lopez <rafael.iskoat@gmail.com>
2020-12-21 14:51:31 -0600

    Agregamos un hook para los likes

--- src/Components/PhotoCard/index.js
+++ src/Components/PhotoCard/index.js
@@ -1,12 +1,15 @@
 import React, { useRef } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 import { useIntersectionObserver } from "@hooks/useIntersectionObserver";
+import { useLocalStorage } from "@hooks/useLocalStorage";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_hamsters.jpg'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const articleRef = useRef(null)
   const { show } = useIntersectionObserver(articleRef)
+  const [liked, setLiked] = useLocalStorage(`like-${id}`, likes)
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={articleRef}>
@@ -18,8 +21,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} alt="image" />
             </ImgWrapper>
           </a>
-          <Button>
-            <MdFavoriteBorder size='32px' /> {likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='32px' /> {likes} likes!
           </Button>
         </>
       }

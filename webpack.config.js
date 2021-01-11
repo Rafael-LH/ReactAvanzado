b84152bece0423b52d8be6daf307faca10c67958
Rafael Lopez <rafael.iskoat@gmail.com>
2021-01-11 14:07:20 -0600

    Configurando nuestra app para que sea PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
 
 module.exports = (_, argv) => {
   console.log(argv.mode)
@@ -42,7 +43,23 @@ module.exports = (_, argv) => {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'public/index.html'),
         filename: 'index.html'
-      })
+      }),
+      new WebpackPwaManifestPlugin({
+        name: 'Petgram tu app de fotos de mascotas.',
+        shotname: 'Petgram 🐶',
+        description: 'Fotos de animalitos bonitos',
+        background_color: '#fff',
+        theme_color: '#b1a',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icons'),
+            purpose: "any maskable",
+            ios: true,
+          }
+        ]
+      }),
     ]
   }
 }
